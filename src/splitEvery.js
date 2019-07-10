4b40e9972b89c452aba28bcb6dcd44237e3ae10e
Andrey Melnikov <andrey.melnikov@csssr.com>
2019-07-11 01:05:53 +0300

    feat(build): add splitEvery

---
+++ src/splitEvery.js
@@ -0,0 +1,11 @@
+export default function splitEvery(n, list) {
+    if (n <= 0) {
+      throw new Error('First argument to splitEvery must be a positive integer');
+    }
+    var result = [];
+    var idx = 0;
+    while (idx < list.length) {
+      result.push(Array.prototype.slice.call(list, idx, idx += n));
+    }
+    return result;
+};
