21d5347765c6ec588b08248650b19cef5535e91b
Obraztsov Ivan <o6raztsv@gmail.com>
2021-02-28 21:14:25 +0300

    fix: allow provide empty list

--- src/maxBy.js
+++ src/maxBy.js
@@ -1,3 +1,3 @@
-export default (getter, list) => list.reduce(
-    (acc, item) => getter(acc) < getter(item) ? item : acc 
-);
+export default (getter, list) => list && list.length
+  ? list.reduce((acc, item) => getter(acc) < getter(item) ? item : acc)
+  : {};
